f95d926339ae2bc7a618caf2af36159be7d6f9bc
Jason Hancock <jason@jasonhancock.com>
2023-08-11 11:19:31 -0700

    Use log/slog as the underlying logger

--- logger.go
+++ logger.go
@@ -1,13 +1,15 @@
 package logger
 
 import (
+	"context"
 	"fmt"
 	"io"
+	"log/slog"
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 
-	"github.com/go-kit/log"
 	"github.com/go-stack/stack"
 	"github.com/hashicorp/go-multierror"
 )
@@ -18,52 +20,36 @@ const (
 	FormatJSON   = "json"
 )
 
-// Level represents the logging level
-type Level int8
-
-// String returns the string representation of a level.
-func (l Level) String() string {
-	name, ok := levelNames[l]
-	if !ok {
-		return levelNames[All]
-	}
-	return name
-}
-
-var levelNames = map[Level]string{
-	All:   "all",
-	Fatal: "fatal",
-	Err:   "err",
-	Warn:  "warn",
-	Info:  "info",
-	Debug: "debug",
-}
-
-// Level constants
 const (
-	All Level = iota
-	Fatal
-	Err
-	Warn
-	Info
-	Debug
+	LevelAll   = slog.Level(-10)
+	LevelFatal = slog.Level(12)
 )
 
+var levelNames = map[slog.Leveler]string{
+	LevelAll:        "all",
+	LevelFatal:      "fatal",
+	slog.LevelError: "err",
+	slog.LevelWarn:  "warn",
+	slog.LevelInfo:  "info",
+	slog.LevelDebug: "debug",
+}
+
 // ParseLevel parses the string into a Level.
-func ParseLevel(s string) Level {
+func ParseLevel(s string) slog.Leveler {
+	s = strings.ToLower(s)
 	for l, name := range levelNames {
-		if strings.HasPrefix(name, strings.ToLower(s)) {
+		if strings.HasPrefix(name, s) {
 			return l
 		}
 	}
-	return All
+	return LevelAll
 }
 
 // L is the logger implementation
 type L struct {
-	logger log.Logger
-	Level  Level
-	src    []string
+	slogger    *slog.Logger
+	src        []string
+	showCaller bool
 }
 
 // New initializes a new logger. If w is nil, logs will be sent to stdout.
@@ -71,107 +57,128 @@ func New(opts ...Option) *L {
 	opt := &options{
 		destination: os.Stdout,
 		name:        filepath.Base(os.Args[0]),
+		showCaller:  true,
 	}
 
 	for _, o := range opts {
 		o(opt)
 	}
 
-	var l log.Logger
+	if opt.timeFormatter == nil {
+		// Detect if the current Location is UTC or not. If not, install the formatter.
+		// This is an optimization because servers should be set to UTC.
+		ts := time.Now()
+		if ts.Format(time.RFC3339Nano) != ts.In(time.UTC).Format(time.RFC3339Nano) {
+			opt.timeFormatter = func(ts time.Time) string {
+				return ts.In(time.UTC).Format(time.RFC3339Nano)
+			}
+		}
+	}
+
+	var l *slog.Logger
+
+	handlerOpts := slog.HandlerOptions{
+		Level: ParseLevel(opt.level),
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			switch a.Key {
+			case slog.TimeKey:
+				a.Key = "ts"
+				if opt.timeFormatter != nil {
+					a.Value = slog.StringValue(opt.timeFormatter(a.Value.Time()))
+				}
+			case slog.LevelKey:
+				level := a.Value.Any().(slog.Level)
+				levelLabel, exists := levelNames[level]
+				if !exists {
+					levelLabel = level.String()
+				}
+				a.Value = slog.StringValue(levelLabel)
+			default:
+			}
+
+			return a
+
+		},
+	}
 
 	switch strings.ToLower(opt.format) {
 	case FormatJSON:
-		l = log.NewJSONLogger(log.NewSyncWriter(opt.destination))
+		l = slog.New(slog.NewJSONHandler(opt.destination, &handlerOpts))
 	default:
-		l = log.NewLogfmtLogger(log.NewSyncWriter(opt.destination))
+		l = slog.New(slog.NewTextHandler(opt.destination, &handlerOpts))
 	}
 
-	l = log.With(
-		l,
-		"ts", log.DefaultTimestampUTC,
-		"caller", caller(5),
-	)
-
-	if len(opt.keyvals) > 0 {
-		l = log.With(l, opt.keyvals...)
-	}
+	l = l.With(append(opt.keyvals, slog.String("src", opt.name))...)
 
 	return &L{
-		logger: l,
-		Level:  ParseLevel(opt.level),
-		src:    []string{opt.name},
+		slogger:    l,
+		src:        []string{opt.name},
+		showCaller: opt.showCaller,
 	}
 }
 
-// Caller returns a log.Valuer that returns a file and line from a specified depth
+// caller returns a string that returns a file and line from a specified depth
 // in the callstack.
-func caller(depth int) log.Valuer {
-	return func() interface{} {
-		c := stack.Caller(depth)
-		// The format string here has special meaning. See
-		// https://godoc.org/github.com/go-stack/stack#Call.Format
-		return fmt.Sprintf("%+k/%s:%d", c, c, c)
-	}
+func caller(depth int) string {
+	c := stack.Caller(depth)
+	// The format string here has special meaning. See
+	// https://godoc.org/github.com/go-stack/stack#Call.Format
+	return fmt.Sprintf("%+k/%s:%d", c, c, c)
 }
 
 // New returns a sub-logger with the name appended to the existing logger's source
 func (l *L) New(name string) *L {
 	return &L{
-		src:    append(l.src, name),
-		Level:  l.Level,
-		logger: l.logger,
+		src:     append(l.src, name),
+		slogger: l.slogger.With(slog.String("src", strings.Join(append(l.src, name), "."))),
 	}
 }
 
 // With returns a logger with the keyvals appended to the existing logger
-func (l *L) With(keyvals ...interface{}) *L {
+func (l *L) With(keyvals ...any) *L {
 	return &L{
-		src:    l.src,
-		Level:  l.Level,
-		logger: log.With(l.logger, keyvals...),
+		src:     l.src,
+		slogger: l.slogger.With(keyvals...),
 	}
 }
 
 // Debug logs a message at the debug level
-func (l *L) Debug(msg interface{}, keyvals ...interface{}) {
-	l.log(Debug, log.With(l.logger, "src", l.source(), "level", Debug.String(), "msg", msg), keyvals...)
+func (l *L) Debug(msg string, keyvals ...any) {
+	l.log(context.Background(), slog.LevelDebug, msg, keyvals...)
 }
 
 // Info logs a message at the info level
-func (l *L) Info(msg interface{}, keyvals ...interface{}) {
-	l.log(Info, log.With(l.logger, "src", l.source(), "level", Info.String(), "msg", msg), keyvals...)
+func (l *L) Info(msg string, keyvals ...any) {
+	l.log(context.Background(), slog.LevelInfo, msg, keyvals...)
 }
 
 // Warn logs a message at the warning level
-func (l *L) Warn(msg interface{}, keyvals ...interface{}) {
-	l.log(Warn, log.With(l.logger, "src", l.source(), "level", Warn.String(), "msg", msg), keyvals...)
+func (l *L) Warn(msg string, keyvals ...any) {
+	l.log(context.Background(), slog.LevelWarn, msg, keyvals...)
 }
 
 // Err logs a message at the error level
-func (l *L) Err(msg interface{}, keyvals ...interface{}) {
-	l.log(Err, log.With(l.logger, "src", l.source(), "level", Err.String(), "msg", msg), keyvals...)
+func (l *L) Err(msg string, keyvals ...any) {
+	l.log(context.Background(), slog.LevelError, msg, keyvals...)
 }
 
 // Fatal logs a message at the fatal level and also exits the program by calling
 // os.Exit
-func (l *L) Fatal(msg interface{}, keyvals ...interface{}) {
-	l.log(Fatal, log.With(l.logger, "src", l.source(), "level", Fatal.String(), "msg", msg), keyvals...)
+func (l *L) Fatal(msg string, keyvals ...any) {
+	l.log(context.Background(), LevelFatal, msg, keyvals...)
 	os.Exit(1)
 }
 
-func (l *L) source() string {
-	return strings.Join(l.src, ".")
-}
-
-func (l *L) log(level Level, lvl log.Logger, keyvals ...interface{}) {
+func (l *L) log(ctx context.Context, lvl slog.Level, msg string, keyvals ...any) {
 	if l == nil {
 		return
 	}
-	if level > l.Level && l.Level != All {
-		return
+
+	if l.showCaller {
+		keyvals = append(keyvals, slog.String("caller", caller(3)))
 	}
 
-	_ = lvl.Log(keyvals...)
+	l.slogger.Log(ctx, lvl, msg, keyvals...)
 }
 
 // Default returns a default logger implementation
@@ -195,17 +202,19 @@ func Silence() *L {
 func (l *L) LogError(msg string, err error) {
 	mErr, ok := err.(*multierror.Error)
 	if !ok {
-		l.Err(msg, "error", err.Error())
+		l.Err(msg, slog.String("error", err.Error()))
 		return
 	}
 
-	keyvals := make([]interface{}, 0, len(mErr.Errors)*2)
+	keyvals := make([]any, 0, len(mErr.Errors))
 
 	for i, e := range mErr.Errors {
 		keyvals = append(
 			keyvals,
-			fmt.Sprintf("error_%02d", i),
-			e.Error(),
+			slog.String(
+				fmt.Sprintf("error_%02d", i),
+				e.Error(),
+			),
 		)
 	}
 
--- options.go
+++ options.go
@@ -1,14 +1,21 @@
 package logger
 
-import "io"
+import (
+	"io"
+	"time"
+)
 
 type options struct {
-	format      string
-	name        string
-	keyvals     []interface{}
-	level       string
-	destination io.Writer
+	format        string
+	name          string
+	keyvals       []interface{}
+	level         string
+	destination   io.Writer
+	showCaller    bool
+	timeFormatter TimeFormatterFunc
 }
+
+type TimeFormatterFunc func(time.Time) string
 
 // Option is used to customize the logger.
 type Option func(*options)
@@ -41,5 +48,19 @@ func WithDestination(w io.Writer) Option {
 func WithName(name string) Option {
 	return func(o *options) {
 		o.name = name
+	}
+}
+
+func WithCaller(showCaller bool) Option {
+	return func(o *options) {
+		o.showCaller = showCaller
+	}
+}
+
+func WithTimeLocation(loc *time.Location) Option {
+	return func(o *options) {
+		o.timeFormatter = func(ts time.Time) string {
+			return ts.In(loc).Format(time.RFC3339Nano)
+		}
 	}
 }
